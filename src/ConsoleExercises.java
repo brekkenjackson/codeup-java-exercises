2236b4b3a5cfe282075975d673c9e108933634aa
brekkenjackson <106278680+brekkenjackson@users.noreply.github.com>
2022-08-02 11:05:38 -0500

    completed ConsoleExercises.java

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,48 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %s", pi);
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int userInput = scanner.nextInt();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//
+//
+//
+//        Scanner s = new Scanner(System.in);
+//        System.out.print("Enter 3 words: ");
+//        String wordOne = s.next();
+//        String wordTwo = s.next();
+//        String wordThree = s.next();
+//        System.out.printf("%s" + "%n" + "%s" + "%n" + "%s" + "%n", wordOne, wordTwo, wordThree);
+
+
+
+//        Scanner newScan = new Scanner(System.in);
+//        System.out.print("Enter a sentence: ");
+//        String niceSentence = newScan.nextLine();
+//        System.out.println(niceSentence);
+
+
+//        Scanner area = new Scanner(System.in);
+//        System.out.print("Enter the height: ");
+//        int myHeight = area.nextInt();
+//        System.out.print("Enter the width: ");
+//        int myWidth = area.nextInt();
+//        System.out.printf("The area is: %s %n", myHeight * myWidth);
+
+
+        Scanner perimeter = new Scanner(System.in);
+        System.out.print("Enter the height: ");
+        int periHeight = perimeter.nextInt();
+        System.out.print("Enter the width: ");
+        int periWidth = perimeter.nextInt();
+        System.out.printf("The perimeter is: %s %n", periHeight*2 + periWidth*2);
+
+    }
+}
