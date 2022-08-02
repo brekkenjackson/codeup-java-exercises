af7bde163dc11e58d6c437a772aaf35e63fc0d02
brekkenjackson <106278680+brekkenjackson@users.noreply.github.com>
2022-08-02 13:42:13 -0500

    touch up on 1st prompt after lecture

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,10 +1,11 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
 //        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %s", pi);
+//        System.out.printf("The value of pi is approximately: ");
+//        System.out.format("%.2f", pi);
+
 
 
 //        Scanner scanner = new Scanner(System.in);
